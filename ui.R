3f58b800467c21ed5a5dfad49162a91eec6ccab0
martinjhnhadley <martinjohnhadley@gmail.com>
2018-03-16 14:03:43 +0000

    Added links and switched to navbarPage

--- ui.R
+++ ui.R
@@ -10,46 +10,74 @@
 
 library(shiny)
 
-shinyUI(fluidPage(
-  title="Simple Histogram",
-  # Application title
-  #titlePanel("Old Faithful Geyser Data"),
+shinyUI(navbarPage(
+  "Simple Histogram",
+  tabPanel(
+    "Visualisation",
+    fluidPage(
+      title = "Simple Histogram",
+      # Application title
+      # titlePanel("Old Faithful Geyser Data"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
+      # Sidebar with a slider input for number of bins
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput(
+            "bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  ),
-  p(
-    tagList(
-      "Created using the ",
-      a(
-        "Shiny Demo app",
-        href = "https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/",
-        target = "_blank"
-      ),
-      " for the ",
-      a(
-        "MSc in Evidence-Based Health Care",
-        href = "https://www.conted.ox.ac.uk/about/msc-in-evidence-based-health-care",
-        target = "_blank"
-      ),
-      " at the ",
-      a(
-        "University of Oxford",
-        href = "https://www.ox.ac.uk",
-        target = "_blank"
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("distPlot")
+        )
       )
     )
-  )
+  ),
+  tabPanel(
+    "About",
+    fluidPage(
+      p(
+        tagList(
+          "This app was created using the ",
+          a(
+            "Shiny Demo app",
+            href = "https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/",
+            target = "_blank"
+          ),
+          " for the ",
+          a(
+            "MSc in Evidence-Based Health Care",
+            href = "https://www.conted.ox.ac.uk/about/msc-in-evidence-based-health-care",
+            target = "_blank"
+          ),
+          " at the ",
+          a(
+            "University of Oxford",
+            href = "https://www.ox.ac.uk",
+            target = "_blank"
+          )
+        )
+      ),
+      p(
+        "This Shiny app is hosted on the University of Oxford's ",
+        a(
+          "Interactive Data Network",
+          href = "http://idn.it.ox.ac.uk",
+          target = "_blank"
+        ),
+        " shinyapps.io subscription."
+      ),
+      p(strong("Author: "), "John Mason"),
+      p(strong("Code Deposit: "), a(
+        "https://doi.org/10.6084/m9.figshare.5966242",
+        href = "https://doi.org/10.6084/m9.figshare.5966242",
+        target = "_blank"
+      ))
+    )
+  ), collapsible = TRUE
 ))
